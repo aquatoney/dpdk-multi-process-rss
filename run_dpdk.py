b912a581cc93cd9fe2513fdc3c8a9ba30b48a9cf
Hao Li <aquatoney@gmail.com>
2020-02-23 14:55:52 +0800

    Update run_dpdk.py

--- run_dpdk.py
+++ run_dpdk.py
@@ -5,24 +5,24 @@ import argparse
 proc_list = []
 
 def cmd(exe, cpu_range, port_range, gid):
-  port_binary_list = ''.join(['1' if p in port_range else '0' for p in range(0,8)])[::-1]
+  port_binary_str = ''.join(['1' if p in port_range else '0' for p in range(0,8)])[::-1]
   # port_binary_str = '0' * (8-ports) + '1' * ports
   port_hex_str = hex(int(port_binary_str, 2))
   thread_num = len(cpu_range)
-  for i in thread_num:
+  for i in range(thread_num):
     exe_str = 'sudo ' + exe
     arg_str =  f' -l {cpu_range[i]} -n 4 --proc-type=auto --file-prefix=g{gid}'
     arg_str += f' -- -p {port_hex_str} --num-procs={thread_num} --proc-id={i}'
     # arg_str += ' &'
     print(exe_str+arg_str)
-    while True:
-      proc = subprocess.Popen([exe_str+arg_str], shell=True)
-      # os.system(exe_str+arg_str)
-      c = input()
-      if c == '':
-        proc_list.append(proc)
-        break
-      proc.terminate()
+    # while True:
+    #   proc = subprocess.Popen([exe_str+arg_str], shell=True)
+    #   # os.system(exe_str+arg_str)
+    #   c = input()
+    #   if c == '':
+    #     proc_list.append(proc)
+    #     break
+    #   proc.terminate()
 
   print(f'All processes in Group {gid} are running')
 
@@ -38,6 +38,7 @@ def kill(exe):
   # os.system(cmd_str)
 
 def exit(signum, frame):
+  global to_kill
   print('Ready to exit.')
   to_kill = True
 
@@ -46,17 +47,18 @@ def str2range(s):
   end = int(s.split('-')[1])
   return range(start, end+1)
 
-# python3 run_dpdk.py build/symmetric_mp {'c':'1-4','p':'0-1'} {'c':'5-8','p':'2-3'}
+# python3 run_dpdk.py build/symmetric_mp {c:1-4,p:0-1} {c:5-8,p:2-3}
 if __name__ == '__main__':
   assert len(sys.argv) > 2
   signal.signal(signal.SIGINT, exit)
   exe = sys.argv[1]
 
+
   for i in range(2, len(sys.argv)):
     cpu_range = port_range = None
     try:
       cp_map = dict()
-      cp_str = sys.argv[i]
+      cp_str = sys.argv[i][1:-1]
       for p in cp_str.split(','):
         key = p.split(':')[0]
         value = p.split(':')[1]
@@ -68,7 +70,7 @@ if __name__ == '__main__':
       print (f'Wrong format of group: {sys.argv[i]}')
       exit()
     assert cpu_range is not None and port_range is not None
-    cmd (exe, cpu_range, port_range, i)
+    cmd (exe, cpu_range, port_range, i-1)
 
   print('All processes are running')
 
